54b69e2e31eeda217efd96cadd4a1f4a82fb9b8a
Soma Zsják <sanraith@users.noreply.github.com>
2024-12-06 19:29:14 +0100

    Simplified Day 06 solution.

--- shared/src/main/scala/hu/sanraith/aoc2024/solution/Day06.scala
+++ shared/src/main/scala/hu/sanraith/aoc2024/solution/Day06.scala
@@ -6,71 +6,54 @@ import scala.collection.mutable
 class Day06 extends Solution:
   override val title: String = "Guard Gallivant"
 
-  val directions = Seq(Point(0, -1), Point(1, 0), Point(0, 1), Point(-1, 0)) // N, E, S, W
-  val StartTile = '^';
-  val EmptyTile = '.';
-  val ObstructionTile = '#';
+  val DIRECTIONS = Seq(Point(0, -1), Point(1, 0), Point(0, 1), Point(-1, 0)) // N, E, S, W
+  val START_TILE = '^'
+  val EMPTY_TILE = '.'
+  val OBSTACLE_TILE = '#'
 
   override def part1(ctx: Context): Long =
     val grid = parseInput(ctx)
-    val visited = getVisitedTiles(grid)
-    visited.size
+    val (route, _) = findRoute(grid)
+    route.map { case (pos, dir) => pos }.size
 
   override def part2(ctx: Context): Long =
     val grid = parseInput(ctx)
-    val tiles = grid.tiles
-    val start = tiles.find { case (_, tile) => tile == StartTile }.map { case (p, _) => p }.get
+    val (route, _) = findRoute(grid)
+    val routeSteps = route.map { case (pos, dir) => pos } - grid.start
+    routeSteps.toSeq.zipWithIndex.count:
+      case (step, idx) =>
+        ctx.progress(idx.toDouble / routeSteps.size)
+        val (_, isLoop) = findRoute(grid, Some(grid.tiles + (step -> OBSTACLE_TILE)))
+        isLoop
 
-    val possibleObstaclePositions = getVisitedTiles(grid) - start
-    possibleObstaclePositions.toSeq.zipWithIndex.count { case (extraObstaclePos, idx) =>
-      ctx.progress(idx.toDouble / possibleObstaclePositions.size)
-      val tiles2 = tiles + (extraObstaclePos -> ObstructionTile)
+  def findRoute(grid: Grid, tilesOverride: Option[Map[Point, Char]] = None) //
+      : (mutable.Set[(Point, Point)], Boolean) =
+    val tiles = tilesOverride.getOrElse(grid.tiles)
+    val route = mutable.Set.empty[(Point, Point)]
+    var pos = grid.start
+    var dirIdx = 0
+    var isLoop = false
 
-      var pos = start
-      var dir = directions(0)
-      val visited = mutable.Set.empty[(Point, Point)]
-      var isLoop = false
+    while (!isLoop && grid.isInBounds(pos))
+      isLoop = !route.add(pos, DIRECTIONS(dirIdx))
+      val nextPos = pos + DIRECTIONS(dirIdx)
+      tiles.get(nextPos) match
+        case Some(OBSTACLE_TILE) => dirIdx = (dirIdx + 1) % DIRECTIONS.size
+        case _                   => pos = nextPos
 
-      while (!isLoop && grid.isInside(pos)) {
-        isLoop = !visited.add(pos, dir)
-        val next = pos + dir
-        tiles2.get(next) match {
-          case tile if tile == Some(ObstructionTile) =>
-            dir = directions((directions.indexOf(dir) + 1) % directions.length)
-          case _ => pos = next
-        }
-      }
-      isLoop
-    }
-
-  def getVisitedTiles(grid: Grid): Set[Point] =
-    val tiles = grid.tiles
-    val start = tiles.find { case (_, tile) => tile == StartTile }.map { case (p, _) => p }.get
-
-    var pos = start
-    var dir = directions(0)
-    val visited = mutable.Set.empty[Point]
-    while (grid.isInside(pos)) {
-      visited.add(pos)
-      val next = pos + dir
-      tiles.get(next) match {
-        case Some(ObstructionTile) =>
-          dir = directions((directions.indexOf(dir) + 1) % directions.length)
-        case _ => pos = next
-      }
-    }
-    visited.toSet
+    (route, isLoop)
 
   def parseInput(ctx: Context): Grid =
     val tiles = ctx.input.linesIterator.zipWithIndex.flatMap { case (line, y) =>
-      line.zipWithIndex.map { case (tile, x) => Point(x, y) -> tile }
+      line.zipWithIndex.collect { case (tile, x) => Point(x, y) -> tile }
     }.toMap
     Grid(tiles)
 
   case class Grid(val tiles: Map[Point, Char]):
+    val start = tiles.collectFirst { case (p, START_TILE) => p }.get
     val width = tiles.keys.map(_.x).max + 1
     val height = tiles.keys.map(_.y).max + 1
-    def isInside(p: Point): Boolean = p.x >= 0 && p.x < width && p.y >= 0 && p.y < height
+    def isInBounds(p: Point): Boolean = p.x >= 0 && p.x < width && p.y >= 0 && p.y < height
 
   case class Point(x: Int, y: Int):
     def +(other: Point): Point = Point(this.x + other.x, this.y + other.y)
