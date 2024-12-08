6348355fd2c3da3a7572f1d0d862a24adfbaa268
Soma Zsják <sanraith@users.noreply.github.com>
2024-12-08 16:05:35 +0100

    Simplified Day 08 solution.

--- shared/src/main/scala/hu/sanraith/aoc2024/solution/Day08.scala
+++ shared/src/main/scala/hu/sanraith/aoc2024/solution/Day08.scala
@@ -10,47 +10,38 @@ class Day08 extends Solution:
 
   override def part1(ctx: Context): Long =
     val grid = parseInput(ctx)
-    val antennas =
-      grid.tiles.groupBy { case (_, tile) => tile }.mapValues(_.map { case (p, _) => p })
-    val antiNodes = antennas.flatMap { case (tile, positions) =>
-      positions.toSeq
-        .combinations(2)
-        .flatMap { case Seq(a, b) =>
-          val dir = b - a
-          Seq(a - dir, b + dir).filter(grid.isInBounds)
-        }
-    }.toSet
+    val antennas = grid.tiles.groupBy { case (_, tile) => tile }.mapValues(_.keySet)
+    val antiNodes = antennas.toSet.flatMap:
+      case (_, antennaTypePositions) =>
+        val antennaPairs = antennaTypePositions.toSeq.combinations(2)
+        antennaPairs.flatMap:
+          case Seq(a, b) =>
+            val dir = b - a
+            Seq(a - dir, b + dir).filter(grid.isInBounds)
     antiNodes.size
 
   override def part2(ctx: Context): Long =
     val grid = parseInput(ctx)
-    val antennas =
-      grid.tiles.groupBy { case (_, tile) => tile }.mapValues(_.map { case (p, _) => p })
-    val antiNodes = antennas.flatMap { case (tile, positions) =>
-      positions.toSeq
-        .combinations(2)
-        .flatMap { case Seq(a, b) =>
-          val dir = b - a
-          var dist = dir
-          var isInBounds = true
-          val groupedNodes = mutable.Set.empty[Point]
-          while (isInBounds)
-            val newNodes = Set(a + dist, b - dist).filter(grid.isInBounds)
-            groupedNodes.addAll(newNodes)
-            isInBounds = newNodes.nonEmpty
-            dist += dir
-
-          groupedNodes
-        }
-    }.toSet
-
+    val antennas = grid.tiles.groupBy { case (_, tile) => tile }.mapValues(_.keySet)
+    val antiNodes = antennas.toSet.flatMap:
+      case (_, antennaTypePositions) =>
+        val antennaPairs = antennaTypePositions.toSeq.combinations(2)
+        antennaPairs.flatMap:
+          case Seq(a, b) =>
+            val dir = b - a
+            (0 until Int.MaxValue).iterator
+              .map(dist => Seq(a - dir * dist, b + dir * dist).filter(grid.isInBounds))
+              .takeWhile(_.nonEmpty)
+              .flatten
     antiNodes.size
 
   def parseInput(ctx: Context) =
     val tiles = ctx.input.linesIterator.zipWithIndex.flatMap { case (line, y) =>
       line.zipWithIndex.collect { case (tile, x) if tile != EMPTY_TILE => Point(x, y) -> tile }
     }.toMap
-    Grid(tiles, ctx.input.linesIterator.toSeq.head.length, ctx.input.linesIterator.toSeq.length)
+    val width = ctx.input.linesIterator.toSeq.headOption.map(_.length).getOrElse(0)
+    val height = ctx.input.linesIterator.toSeq.length
+    Grid(tiles, width, height)
 
   case class Grid(val tiles: Map[Point, Char], width: Int, height: Int):
     def isInBounds(p: Point): Boolean = p.x >= 0 && p.x < width && p.y >= 0 && p.y < height
