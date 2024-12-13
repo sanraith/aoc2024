e84285c30e8f718370191a9f7c9c620097bec389
Soma Zsják <sanraith@users.noreply.github.com>
2024-12-13 12:15:19 +0100

    Added Day 13 solution.

--- shared/src/main/scala/hu/sanraith/aoc2024/solution/Day13.scala
+++ shared/src/main/scala/hu/sanraith/aoc2024/solution/Day13.scala
@@ -1,7 +1,7 @@
 package hu.sanraith.aoc2024.solution
 
-import hu.sanraith.aoc2024.util._
 import scala.collection.mutable
+import scala.math.Integral.Implicits.*
 
 /** Solution for https://adventofcode.com/2024/day/13 */
 class Day13 extends Solution:
@@ -9,63 +9,37 @@ class Day13 extends Solution:
 
   override def part1(ctx: Context): Long =
     val machines = parseInput(ctx)
-    getWithDelta(machines, ctx)
-    // val maxPressCount = 100
-    // val machines = parseInput(ctx)
-    // machines.map { machine =>
-    //   val costs = for
-    //     timesB <- 0 to maxPressCount
-    //     timesA <- 0 to maxPressCount
-    //     if machine.a * timesA + machine.b * timesB == machine.prize
-    //   yield timesA * 3 + timesB
-    //   costs.minOption.getOrElse(0)
-    // }.sum
+    countTokens(machines, ctx)
 
   override def part2(ctx: Context): Long =
     val delta = Point(10000000000000L, 10000000000000L)
     val machines = parseInput(ctx)
-    getWithDelta(machines, ctx, delta)
+    countTokens(machines, ctx, delta)
 
-    // 91204896407640 too high
+  def countTokens(machines: Seq[ClawMachine], ctx: Context, delta: Point = Point(0, 0)) = machines
+    .map { case ClawMachine(a, b, p) => ClawMachine(a, b, p + delta) }
+    .map { case ClawMachine(a, b, p) =>
+      // px = ax * n + bx * m
+      // py = ay * n + by * m
+      // solve for single root n, m
+      // n = (by * px - nx * py) / (ax * by - ay * bx)
+      // m = (ay * px - ax * py) / (ay * bx - ax * by)
+      val (Point(ax, ay), Point(bx, by), Point(px, py)) = (a, b, p)
+      val (n, nr) = (by * px - bx * py) /% (ax * by - ay * bx)
+      val (m, mr) = (ay * px - ax * py) /% (ay * bx - ax * by)
+      val hasSolution = n >= 0 && m >= 0 && nr == 0 && mr == 0
 
-  def getWithDelta(machines: Seq[ClawMachine], ctx: Context, delta: Point = Point(0, 0)) =
-    machines
-      .map { case ClawMachine(a, b, p) => ClawMachine(a, b, p + delta) }
-      .iterator
-      .tapEachWithIndex { case (_, idx) => ctx.progress(idx.toDouble / machines.length) }
-      .map { case ClawMachine(a, b, p) =>
-        val visited = mutable.Set.empty[Point]
-        var n = Math.min(p.x / b.x, p.y / b.y)
-        var found: Option[Long] = None
-        var shouldContinue = true
-        while (found.isEmpty && shouldContinue && n >= 0) {
-          val rem = (p - b * n) % a
-          if (rem.x == 0 && rem.y == 0) found = Some(n)
-          if (!visited.add(rem)) shouldContinue = false
-          n -= 1
-        }
-
-        found match
-          case Some(nb) =>
-            val na = (p - b * nb).x / a.x
-            // println(s"B times $nb, A times $na");
-            na * 3 + nb
-          case None => 0
-      }
-      .sum
+      // My inputs only had a single root, so this produces the correct result even without caring about the token weights
+      if (hasSolution) n * 3 + m else 0
+    }
+    .sum
 
   def parseInput(ctx: Context): Seq[ClawMachine] =
-    val numPairRegex = """(\d+).*?(\d+)""".r
-    ctx.input.split("""\R\R""").toSeq.map { case m =>
-      numPairRegex
-        .findAllMatchIn(m)
-        .map { case numPairRegex(x, y) => Point(x.toLong, y.toLong) }
-        .toSeq match
-        case Seq(a, b, p) => ClawMachine(a, b, p)
-    }
-
-  def lcm(a: Long, b: Long): Long = a * b / gcd(a, b)
-  def gcd(a: Long, b: Long): Long = if b == 0 then a else gcd(b, a % b)
+    val posRgx = """(\d+).*?(\d+)""".r
+    ctx.input
+      .split("""\R\R""")
+      .map(posRgx.findAllMatchIn(_).toSeq.map { case posRgx(x, y) => Point(x.toLong, y.toLong) })
+      .collect { case Seq(a, b, p) => ClawMachine(a, b, p) }
 
   case class ClawMachine(a: Point, b: Point, prize: Point)
 
@@ -73,4 +47,3 @@ class Day13 extends Solution:
     def +(other: Point): Point = Point(this.x + other.x, this.y + other.y)
     def -(other: Point): Point = Point(this.x - other.x, this.y - other.y)
     def *(scalar: Long): Point = Point(this.x * scalar, this.y * scalar)
-    def %(other: Point): Point = Point(this.x % other.x, this.y % other.y)
