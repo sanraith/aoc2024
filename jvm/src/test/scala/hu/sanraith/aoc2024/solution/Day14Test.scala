d441b617ef4e30f4c22ecb12ea06b820bd436884
Soma Zsják <sanraith@users.noreply.github.com>
2024-12-14 12:06:41 +0100

    Added Day 14 solution.

--- jvm/src/test/scala/hu/sanraith/aoc2024/solution/Day14Test.scala
+++ jvm/src/test/scala/hu/sanraith/aoc2024/solution/Day14Test.scala
@@ -27,18 +27,11 @@ p=7,3 v=-1,2
 p=2,4 v=2,-3
 p=9,5 v=-3,-3"""
       )
-
-    it("solves part 2"):
-      _assertPart(
-        day.part2,
-        expected = "__PART_2_TEST_EXPECTED__",
-        input = """__PART_2_TEST_INPUT__"""
-      )
   }
 
   describe("Day14 for puzzle input") {
     given day: Solution = Day14()
 
     it("solves part 1")(assertPart(day.part1, 231782040L))
-    it("solves part 2")(_assertPart(day.part2, "__PART_2_EXPECTED__"))
+    it("solves part 2")(assertPart(day.part2, 6475))
   }
--- shared/src/main/scala/hu/sanraith/aoc2024/solution/Day14.scala
+++ shared/src/main/scala/hu/sanraith/aoc2024/solution/Day14.scala
@@ -1,12 +1,14 @@
 package hu.sanraith.aoc2024.solution
 
 import hu.sanraith.aoc2024.solution.Day14._
+import scala.collection.mutable
 
 /** Solution for https://adventofcode.com/2024/day/14 */
 class Day14 extends Solution:
   override val title: String = "Restroom Redoubt"
 
   var gridSize = Point(101, 103)
+  val NORTH = Point(0, -1)
 
   override def part1(ctx: Context): Long =
     val guards = parseInput(ctx)
@@ -22,7 +24,40 @@ class Day14 extends Solution:
       .product
 
   override def part2(ctx: Context): Long =
-    ???
+    // The heuristic is looking for a long vertical line that represents the trunk of the tree
+    val minTrunkSize = 20
+    val guards = parseInput(ctx)
+
+    Iterator
+      .from(1)
+      .map: step =>
+        val guardsAtStep = guards.map { case Guard(p, v) => Guard((p + v * step) % gridSize, v) }
+        val hasTrunk = guardsAtStep
+          .sortBy(_.p.y)
+          .groupMap(_.p.x)(_.p)
+          .values
+          .filter(_.length >= minTrunkSize)
+          .exists: points =>
+            val (trunks, trunk) = points.tail.foldLeft(Seq.empty[Int], Seq(points.head)):
+              case ((trunks, trunk), p) if (p + NORTH == trunk.last) => (trunks, trunk :+ p)
+              case ((trunks, trunk), p) => (trunks :+ trunk.length, Seq(p))
+            (trunks :+ trunk.length).exists(_ >= minTrunkSize)
+        // if (hasTrunk) render(guardsAtStep)
+        (step, hasTrunk)
+      .collectFirst { case (step, hasTrunk) if hasTrunk => step }
+      .get
+
+  def render(guards: Seq[Guard]) =
+    val map = guards.groupBy(g => g.p).toMap
+    val rendered = (0 until gridSize.y.toInt)
+      .map: y =>
+        (0 until gridSize.x.toInt)
+          .map: x =>
+            val guardCount = map.get(Point(x, y)).map(_.length).getOrElse(0)
+            if (guardCount > 0) '#' else '.'
+          .mkString
+      .mkString("\n")
+    println(rendered)
 
   def parseInput(ctx: Context): Seq[Guard] =
     val numRegex = """(-?\d+)""".r
